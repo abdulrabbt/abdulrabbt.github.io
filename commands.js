bb996c93488b39e85b256fcbe68e05846367ff75
Abdulrab Bin Talib <44449461+abdulrabbt@users.noreply.github.com>
2020-08-05 14:44:16 +0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -21,21 +21,24 @@ function genUrl(event) {
   var room = genPass(8);
   var password = genPass(14);
   var url = "https://" + event.source.id + "/#" + room + "/" + password + "/";
-  msCrypto.subtle
-    .generateKey({ name: "AES-GCM", length: 128 }, true, ["encrypt", "decrypt"])
-    .then(function (key) {
-      let key_exported = msCrypto.subtle.exportKey("jwk", key);
-      key_exported = JSON.parse(arrayBufferToString(key_exported.result)).k;
+  var crypt = msCrypto.subtle.generateKey(
+    { name: "AES-GCM", length: 128 },
+    true,
+    ["encrypt", "decrypt"]
+  );
+  crypt.oncomplete = function (e) {
+    let key_exported = msCrypto.subtle.exportKey("jwk", e.target.result);
+    key_exported = JSON.parse(arrayBufferToString(key_exported.result)).k;
 
-      Office.context.mailbox.displayNewAppointmentForm({
-        requiredAttendees: [],
-        location: "Online",
-        subject: "Ray Meeting",
-        resources: [],
-        body: "\n\n\n\nJoin Ray Meeting \n " + url + key_exported,
-      });
-      event.completed();
+    Office.context.mailbox.displayNewAppointmentForm({
+      requiredAttendees: [],
+      location: "Online",
+      subject: "Ray Meeting",
+      resources: [],
+      body: "\n\n\n\nJoin Ray Meeting \n " + url + key_exported,
     });
+    event.completed();
+  };
 }
 
 Office.onReady(function () {});
