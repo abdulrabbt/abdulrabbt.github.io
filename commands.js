3117c77a1434a407f05282c4c4d4d95706186e64
Abdulrab Bin Talib <44449461+abdulrabbt@users.noreply.github.com>
2020-08-05 14:12:07 +0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,9 +1,4 @@
-Office.initialize = function() {};
-
-function isIE() {
-  return document.documentMode;
-}
-
+Office.initialize = function () {};
 
 function getRandom() {
   var letters_pool = "23456789ABCDEFGHJKLMNPQRST";
@@ -18,43 +13,28 @@ function genPass(length) {
   return result;
 }
 
-async function generateKey() {
-    console.log("key");
-    var key = await crypto.subtle.generateKey({ name: "AES-GCM", length: 128 }, true, ["encrypt", "decrypt"]);
-    var key_exported = await crypto.subtle.exportKey("jwk", key);
-    return key_exported.k;
-  };
-
-function generateKeyIE() {
-    console.log("key IE");
-    return msCrypto.subtle.generateKey({ name: "AES-GCM", length: 128 }, true, ["encrypt", "decrypt"]).then( function (key) {
-      return msCrypto.subtle.exportKey("jwk", key.result).then(function(key_exported){
-        return JSON.parse(arrayBufferToString(key_exported.result)).k
-      })
-          })
-  }
-
-console.log("test")
-async function genUrl(event) {
+function genUrl(event) {
   var room = genPass(8);
   var password = genPass(14);
-  // var encryptionKey = await generateKey(); //"tlyYLfRxK1YPXaChLQAcPQ";
-  var encryptionKey
-  if(isIE()){
-    encryptionKey = await generateKeyIE()
-  } else {
-    encryptionKey = await generateKey();
-  }
-  var url = "https://" + event.source.id + "/#" + room + "/" + password + "/" + encryptionKey;
+  var encryptionKey = "tlyYLfRxK1YPXaChLQAcPQ"; // await generateKey();
+  var url =
+    "https://" +
+    event.source.id +
+    "/#" +
+    room +
+    "/" +
+    password +
+    "/" +
+    encryptionKey;
 
   Office.context.mailbox.displayNewAppointmentForm({
     requiredAttendees: [],
     location: "Online",
     subject: "Ray Meeting",
     resources: [],
-    body: "\n\n\n\nJoin Ray Meeting \n " + url
+    body: "\n\n\n\nJoin Ray Meeting \n " + url,
   });
   event.completed();
 }
 
-Office.onReady(function() {});
+Office.onReady(function () {});
