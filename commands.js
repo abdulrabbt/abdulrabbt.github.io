400e293d9c6b1bb8614e4bf0752e54386dfa40eb
Abdulrab Bin Talib <44449461+abdulrabbt@users.noreply.github.com>
2020-08-05 11:22:49 +0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,9 +1,22 @@
 Office.initialize = function() {};
 
+function getRandom() {
+  var letters_pool = "23456789ABCDEFGHJKLMNPQRST";
+  return letters_pool[Math.floor(Math.random() * letters_pool.length)];
+}
+
+function genPass(length) {
+  var result = "";
+  for (var i = 0; i < length; ++i) {
+    result += getRandom();
+  }
+  return result;
+}
+
+
 function genUrl(event) {
-  // console.log("TESTING");
-  var room = "room"; // genPass(8);
-  var password = "password"; // genPass(14);
+  var room = genPass(8);
+  var password = genPass(14);
   var encryptionKey = "tlyYLfRxK1YPXaChLQAcPQ"; // await generateKey();
   var url = "https://" + event.source.id + "/#" + room + "/" + password + "/" + encryptionKey;
 
