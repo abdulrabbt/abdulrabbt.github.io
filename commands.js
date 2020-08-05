99b711cc93fa55933b7eca872f8dc9a81bd711b1
Abdulrab Bin Talib <44449461+abdulrabbt@users.noreply.github.com>
2020-08-05 11:20:14 +0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -2,27 +2,19 @@ Office.initialize = function() {};
 
 function genUrl(event) {
   // console.log("TESTING");
-//   var room = "room"; // genPass(8);
-//   var password = "password"; // genPass(14);
-//   var encryptionKey = "tlyYLfRxK1YPXaChLQAcPQ"; // await generateKey();
-//   var url = `https://${event.source.id}/#${room}/${password}/${encryptionKey}`;
+  var room = "room"; // genPass(8);
+  var password = "password"; // genPass(14);
+  var encryptionKey = "tlyYLfRxK1YPXaChLQAcPQ"; // await generateKey();
+  var url = "https://" + event.source.id + "/#" + room + "/" + password + "/" + encryptionKey;
 
   Office.context.mailbox.displayNewAppointmentForm({
     requiredAttendees: [],
     location: "Online",
     subject: "Ray Meeting",
     resources: [],
-    body: "\n\n\n\nJoin Ray Meeting \n"
+    body: "\n\n\n\nJoin Ray Meeting \n " + url
   });
-//   Office.context.mailbox.item.notificationMessages.addAsync("subject", {
-//     type: "informationalMessage",
-//     icon: "Icon.16x16",
-//     message: "Subject: 11",
-//     persistent: false
-//   });
   event.completed();
 }
 
-Office.onReady(function() {
-
-});
+Office.onReady(function() {});
