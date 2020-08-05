7e57f66496c3ca021b0e14bfddeb08610d2b1639
Fahad Alarefi <37724969+createdbyfahad@users.noreply.github.com>
2020-08-05 18:27:28 +0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,20 +1,20 @@
-Office.initialize = function () {};
+Office.initialize = function() {};
 
-function isIE() {
-  return document.documentMode;
-}
+// function isIE() {
+//   return document.documentMode;
+// }
 
 function getRandom() {
   var letters_pool = "23456789ABCDEFGHJKLMNPQRST";
   return letters_pool[Math.floor(Math.random() * letters_pool.length)];
 }
 
-function arrayBufferToString( buffer ) {
-  var binary = '';
-  var bytes = new Uint8Array( buffer );
+function arrayBufferToString(buffer) {
+  var binary = "";
+  var bytes = new Uint8Array(buffer);
   var len = bytes.byteLength;
   for (var i = 0; i < len; i++) {
-      binary += String.fromCharCode( bytes[ i ] );
+    binary += String.fromCharCode(bytes[i]);
   }
   return binary;
 }
@@ -31,40 +31,60 @@ function genUrl(event) {
   var room = genPass(8);
   var password = genPass(14);
   var url = "https://" + event.source.id + "/#" + room + "/" + password + "/";
-  var crypt = msCrypto.subtle.generateKey(
-    { name: "AES-GCM", length: 128 },
-    true,
-    ["encrypt", "decrypt"]
-  );
-  crypt.oncomplete = function (e) {
-    console.log(e)
-//     console.log("111",msCrypto.subtle.exportKey("jwk", e.target.result))
-    var ob = msCrypto.subtle.exportKey("jwk", e.target.result);
-    console.log("ob", ob.result)
-    ob.oncomplete = function(e2) {
-      console.log("from oncomplete", e2); 
-      var result = e2.target.result;
-      var k = JSON.parse(arrayBufferToString(result)).k;
-      var currentEmail = Office.context.mailbox.userProfile.emailAddress;
-      Office.context.mailbox.displayNewAppointmentForm({
-        requiredAttendees: [currentEmail],
-        location: "Online",
-        subject: "Ray Meeting",
-        resources: [],
-        body: "\n\n\n\nJoin Ray Meeting \n " + url + k,
-      });
-      event.completed();
-//       console.log("result", result);
-//       console.log("buffer from oncomplete", arrayBufferToString(result));
-    }
-    
-//     console.log("result",msCrypto.subtle.exportKey("jwk", e.target.result).result)
-//     console.log("buffer",arrayBufferToString(ob.result))
-//     let key_exported = msCrypto.subtle.exportKey("jwk", e.target.result);
-//     key_exported = JSON.parse(arrayBufferToString(key_exported.result)).k;
+  var cryptObt;
+  // If msCrypto is present, then use it
+  if (msCrypto) {
+    // NOTE: msCrypto is only supported in IE11 (+ Outlook for win)
+    cryptObt = msCrypto.subtle.generateKey(
+      { name: "AES-GCM", length: 128 },
+      true,
+      ["encrypt", "decrypt"]
+    );
 
-    
-  };
+    cryptObt.oncomplete = function(e) {
+      var ob = msCrypto.subtle.exportKey("jwk", e.target.result);
+      ob.oncomplete = function(e2) {
+        console.log("from oncomplete", e2);
+        var result = e2.target.result;
+        var k = JSON.parse(arrayBufferToString(result)).k;
+        var currentEmail = Office.context.mailbox.userProfile.emailAddress;
+        Office.context.mailbox.displayNewAppointmentForm({
+          requiredAttendees: [currentEmail],
+          location: "Online",
+          subject: "Ray Meeting",
+          resources: [],
+          body: "\n\n\n\nJoin Ray Meeting \n " + url + k,
+        });
+
+        event.completed();
+      };
+    };
+  } else {
+    cryptObt = crypto.subtle.generateKey(
+      { name: "AES-GCM", length: 128 },
+      true,
+      ["encrypt", "decrypt"]
+    );
+
+    cryptObt.oncomplete = function(e) {
+      var ob = crypto.subtle.exportKey("jwk", e.target.result);
+      ob.oncomplete = function(e2) {
+        console.log("from oncomplete", e2);
+        var result = e2.target.result;
+        var k = JSON.parse(arrayBufferToString(result)).k;
+        var currentEmail = Office.context.mailbox.userProfile.emailAddress;
+        Office.context.mailbox.displayNewAppointmentForm({
+          requiredAttendees: [currentEmail],
+          location: "Online",
+          subject: "Ray Meeting",
+          resources: [],
+          body: "\n\n\n\nJoin Ray Meeting \n " + url + k,
+        });
+
+        event.completed();
+      };
+    };
+  }
 }
 
-Office.onReady(function () {});
+Office.onReady(function() {});
