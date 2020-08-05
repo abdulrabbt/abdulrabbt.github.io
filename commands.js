e8fb0bfa78c9014b3c85581fe6e40b1144d77064
Abdulrab Bin Talib <44449461+abdulrabbt@users.noreply.github.com>
2020-08-05 14:20:07 +0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,4 +1,8 @@
-Office.initialize = function () {};
+Office.initialize = function() {};
+
+function isIE() {
+  return document.documentMode;
+}
 
 function getRandom() {
   var letters_pool = "23456789ABCDEFGHJKLMNPQRST";
@@ -13,28 +17,42 @@ function genPass(length) {
   return result;
 }
 
+// async function generateKey() {
+//   var key = await crypto.subtle.generateKey({ name: "AES-GCM", length: 128 }, true, ["encrypt", "decrypt"]);
+//   var key_exported = await crypto.subtle.exportKey("jwk", key);
+//   return key_exported.k;
+// }
+
+// async function generateKeyIE() {
+//   let key = await msCrypto.subtle.generateKey({ name: "AES-GCM", length: 128 }, true, ["encrypt", "decrypt"]);
+//   key = key.result;
+
+//   let key_exported = msCrypto.subtle.exportKey("jwk", key);
+//   key_exported = JSON.parse(arrayBufferToString(key_exported.result));
+
+//   return key_exported.k;
+// }
+
 function genUrl(event) {
   var room = genPass(8);
   var password = genPass(14);
-  var encryptionKey = "tlyYLfRxK1YPXaChLQAcPQ"; // await generateKey();
-  var url =
-    "https://" +
-    event.source.id +
-    "/#" +
-    room +
-    "/" +
-    password +
-    "/" +
-    encryptionKey;
+  // var encryptionKey = "tlyYLfRxK1YPXaChLQAcPQ";
+  var encryptionKey;
+  if (isIE()) {
+    encryptionKey = "testing ";
+  } else {
+    encryptionKey = "testing IE";
+  }
+  var url = "https://" + event.source.id + "/#" + room + "/" + password + "/" + encryptionKey;
 
   Office.context.mailbox.displayNewAppointmentForm({
     requiredAttendees: [],
     location: "Online",
     subject: "Ray Meeting",
     resources: [],
-    body: "\n\n\n\nJoin Ray Meeting \n " + url,
+    body: "\n\n\n\nJoin Ray Meeting \n " + url
   });
   event.completed();
 }
 
-Office.onReady(function () {});
+Office.onReady(function() {});
