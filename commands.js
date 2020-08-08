b156dce8dc2194ab2ba9e30b4bdeaa76bec32204
Fahad Alarefi <37724969+createdbyfahad@users.noreply.github.com>
2020-08-08 13:42:19 +0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -4,6 +4,10 @@ function isIE() {
   return typeof msCrypto === "object";
 }
 
+function isSupported() {
+  return typeof crypto === "object";
+}
+
 function getRandom() {
   var letters_pool = "23456789ABCDEFGHJKLMNPQRST";
   return letters_pool[Math.floor(Math.random() * letters_pool.length)];
@@ -27,24 +31,56 @@ function genPass(length) {
   return result;
 }
 
-function callback(url, event) {
+function roundToHour(date) {
+  p = 60 * 60 * 1000; // milliseconds in an hour
+  return new Date(Math.round(date.getTime() / p) * p);
+}
+
+function constructURL(domain, room, password, encryption) {
+  return "https://" + domain + "/#" + room + "/" + password + "/" + encryption;
+}
+
+function callback(domain, room, password, encryption, event) {
   var currentEmail = Office.context.mailbox.userProfile.emailAddress;
+  var url = constructURL(domain, room, password, encryption);
+
+  // set the starting date to be beginning of next hour
+  // ends a half hour after
+  var startTime = roundToHour(new Date());
+  var endTime = new Date(start);
+  end.setMinutes(end.getMinutes() + 30);
+
   Office.context.mailbox.displayNewAppointmentForm({
     requiredAttendees: [currentEmail],
     location: "Online",
-    subject: "Ray Meeting",
+    subject: "",
     resources: [],
-    body: "\n\n\n\nJoin Ray Meeting \n " + url,
+    start: startTime,
+    end: endTime,
+    body:
+      "\n-- Do not delete or change any of the following text. -- " +
+      "\nTo join the meeting, check the info and link below: " +
+      "\nMeeting ID (access code): " +
+      room +
+      "\nMeeting password: " +
+      password +
+      "\nMeeting encryption: " +
+      encryption +
+      "\n\nJoin the meeting: " +
+      url +
+      "\nThis meeting is powered by Meet.sa (a SITE product).",
   });
 
   event.completed();
 }
 
 function genUrl(event) {
+  console.log(event);
   var room = genPass(8);
   var password = genPass(14);
-  var url = "https://" + event.source.id + "/#" + room + "/" + password + "/";
+  var domain = event.source.id;
   var cryptObt;
+
   // If msCrypto is present, then use it
   if (isIE()) {
     // NOTE: msCrypto is only supported in IE11 (+ Outlook for win)
@@ -59,10 +95,10 @@ function genUrl(event) {
       ob.oncomplete = function(e2) {
         var result = e2.target.result;
         var k = JSON.parse(arrayBufferToString(result)).k;
-        return callback(url + k, event);
+        return callback(domain, room, password, k, event);
       };
     };
-  } else {
+  } else if (isSupported()) {
     cryptObt = crypto.subtle
       .generateKey({ name: "AES-GCM", length: 128 }, true, [
         "encrypt",
@@ -73,8 +109,11 @@ function genUrl(event) {
       })
       .then(function(e2) {
         var k = e2.k;
-        return callback(url + k, event);
+        return callback(domain, room, password, k, event);
       });
+  } else {
+    // TODO: crypto is not supported
+    return event.completed();
   }
 }
 
