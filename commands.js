f5a82ff42fe77124d1105c1a0bc4d0712b7cd4db
Fahad Alarefi <37724969+createdbyfahad@users.noreply.github.com>
2020-08-08 13:46:02 +0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -48,7 +48,7 @@ function callback(domain, room, password, encryption, event) {
   // ends a half hour after
   var startTime = roundToHour(new Date());
   var endTime = new Date(startTime);
-  end.setMinutes(end.getMinutes() + 30);
+  endTime.setMinutes(endTime.getMinutes() + 30);
 
   Office.context.mailbox.displayNewAppointmentForm({
     requiredAttendees: [currentEmail],
